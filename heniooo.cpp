11715074a9bad400f60f7e2c387793411fe3057f
Heniooo <admin@henicz.pl>
2017-12-08 12:10:49 +0100

    create heniooo.cpp
    xd

---
+++ heniooo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+main() 
+{
+cout << "Imie: Mateusz" << endl;
+cout << "Nazwisko: Henicz" << endl;
+cout << "Lat: 16" << endl;
+cout << "Klasa: 1W" << endl << endl << endl;
+cout << "*************************" << endl;
+return 0;
+}
