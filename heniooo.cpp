273c388e42d7640551d80805a5db9eceae7c95f7
henicz-haker <mateusz@henicz.pl>
2020-06-03 15:03:19 -0700

    Added professioanl hacking service option, updated age
    Updated the age of Mateusz Henicz to reflect his mental state

--- heniooo.cpp
+++ heniooo.cpp
@@ -4,8 +4,9 @@ main()
 {
 cout << "Imie: Mateusz" << endl;
 cout << "Nazwisko: Henicz" << endl;
-cout << "Lat: 16" << endl;
-cout << "Klasa: 1W" << endl << endl << endl;
+cout << "Lat: 2137" << endl;
+cout << "Haker: yes" << endl;
+cout << "Klasa: Tak" << endl << endl << endl;
 cout << "*************************" << endl;
 return 0;
 }
